d0227551aba9d02baa847a459c820389ea2cf817
developer28hemanth0010 <developer.28hemanth0010@gmail.coom>
2021-10-18 11:54:44 +0530

    2nd commit

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-
-import random
+from random import randint
 
 
 app = Flask(__name__,static_url_path='/staticeoifeufy94y9348ty8349tynv98wryyxm230ry03vwefowiefjwio',)
@@ -22,23 +21,28 @@ admin = Admin(app, url="/admin123")
 # HOME PAGE
 @app.route("/",methods=['GET','POST'])
 def home_page():
-    if request.method=='POST':
 
-        id= random.randint(45873,48938)
+
+    if request.method=='POST':
+        id = ''.join(["{}".format(randint(0, 9)) for num in range(0, 10)])
         name=request.form.get('name')
         add=request.form.get('add1')
-        commit= purchases(id=id, name=name, address = add)
+        commit= purchases(id=id, name=name, address=add,)
         db.session.add(commit)
         db.session.commit()
+        return render_template('trial2.html',id=id)
 
-    return render_template('index.html')
+    return render_template('trial4.html')
 
 
 # DATABASE
 class purchases(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(30), primary_key=True)
     name = db.Column(db.String(30))
     address = db.Column(db.String(30))
+    
+    def __repr__(self) -> str:
+        return f"{self.id}- {self.name}- {self.address}"
     
 admin.add_view(ModelView(purchases, db.session))
 
